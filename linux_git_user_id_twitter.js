d29be5cfba53db1f5a8e607d8be6a0b8da01a42d
walid <walid@kali.walid>
2024-08-20 18:08:59 +0100

    update

--- linux_git_user_id_twitter.js
+++ linux_git_user_id_twitter.js
@@ -1,7 +1,11 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-    const browser = await puppeteer.launch( {});
+async function getUserId(username) {
+    // proxy = '35.185.196.38:3128';
+    const browser = await puppeteer.launch( {
+        // headless: false,
+        // args: [`--proxy-server=${proxy}`] 
+    });
     const page = await browser.newPage();
 
     await page.setRequestInterception(true);
@@ -12,19 +16,21 @@ const puppeteer = require('puppeteer');
         request.continue();
     });
 
-    const username = '/MustaphaElasri9';
-    await page.goto('https://www.twitter.com' + username);
+    // const username = '/IoQNqS8666';
+    await page.goto('https://www.x.com/' + username);
 
     await browser.close();
-    const searchString = 'https://pbs.twimg.com/profile_banners/';
+    // const searchString = 'https://api.x.com/graphql/E3opETHurmVJflFsUBVuUQ/UserTweets?variables=%7B%22userId%22%3A%22';
+    const searchString = 'https://api.x.com/graphql/E3opETHurmVJflFsUBVuUQ/UserTweets?variables=%7B%22userId%22%3A%22';
     const foundRequest = requests.find(url => url.includes(searchString));
-
     if (foundRequest) {
         const idStartIndex = foundRequest.indexOf(searchString) + searchString.length;
-        const idEndIndex = foundRequest.indexOf('/', idStartIndex);
+        const idEndIndex = foundRequest.indexOf('%22%', idStartIndex);
         const id = foundRequest.substring(idStartIndex, idEndIndex);
         console.log('Found:', id);
     } else {
         console.log('ID not found.');
     }
-})();
+}
+
+getUserId('zakkabi');
